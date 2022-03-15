736cb8da30c723c26d00f56236f6357f3bdb06e3
Rafael Luz <rafaelluz09@live.com>
2022-03-15 20:12:12 -0300

    Update Betemps' files

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
-public class Main{
-    
-}
+class Main {
+    public static void main(String[] args) {
+      Graph<String,String> g;
+      Digraph<String,String> dg;
+      System.out.println("Compiled!");
+    }
+  }
